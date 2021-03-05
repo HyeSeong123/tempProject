cc782c95deb3d129f6a914cbb57042d5fc98f7f1
HyeSeong123 <banggu1997@gmail.com>
2021-03-05 14:37:30 +0900

    첫 커밋

---
+++ src/test/java/com/codingsepo/example/demo/TempProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingsepo.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
