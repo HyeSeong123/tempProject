6941864177198e165fc547bfb29311c4022b9639
HyeSeong123 <banggu1997@gmail.com>
2021-03-10 17:35:11 +0900

    sql 저장

--- db.sql
+++ db.sql
@@ -20,22 +20,35 @@ INSERT INTO article
     `title` = '게시글1',
     `body` = '내용1'
 
+		SELECT A.num, A.title,
+		IFNULL(M.nickname, "탈퇴회원") AS extra__writer,
+		DATE_FORMAT(A.regDate, '%m-%d'),
+		A.view,
+		B.name AS extra__board
+		FROM article AS A
+		LEFT JOIN `member` AS M
+		ON A.memberNum = M.num
+		LEFT JOIN `board` AS B
+		ON A.boardNum = B.num
+		WHERE 1
 
 INSERT INTO article
     SET regDate= NOW(),
     updateDate = NOW(),
     `title` = '게시글2',
-    `BODY` = '내용2'
+    memberNum = 2,
+    boardNum = 1,
+    `body` = '내용2'
 
 UPDATE article
-SET memberNum = 1
+SET memberNum = 3
 WHERE memberNum = 0
 
 UPDATE article
 SET boardNum = 1
 WHERE boardNum = 0
     
-CREATE TABLE `MEMBER`(
+CREATE TABLE `member`(
     num INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
     updateDate DATETIME NOT NULL,
@@ -129,3 +142,23 @@ INSERT INTO reply
     relTypeCode = 'article',
     `body` = '댓글5'
     
+CREATE TABLE getFile (
+  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, # 번호
+  regDate DATETIME DEFAULT NULL, # 작성날짜
+  updateDate DATETIME DEFAULT NULL, # 갱신날짜
+  delDate DATETIME DEFAULT NULL, # 삭제날짜
+  delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0, # 삭제상태(0:미삭제,1:삭제)
+  relTypeCode CHAR(50) NOT NULL, # 관련 데이터 타입(article, member)
+  relId INT(10) UNSIGNED NOT NULL, # 관련 데이터 번호
+  originFileName VARCHAR(100) NOT NULL, # 업로드 당시의 파일이름
+  fileExt CHAR(10) NOT NULL, # 확장자
+  typeCode CHAR(20) NOT NULL, # 종류코드 (common)
+  type2Code CHAR(20) NOT NULL, # 종류2코드 (attatchment)
+  fileSize INT(10) UNSIGNED NOT NULL, # 파일의 사이즈
+  fileExtTypeCode CHAR(10) NOT NULL, # 파일규격코드(img, video)
+  fileExtType2Code CHAR(10) NOT NULL, # 파일규격2코드(jpg, mp4)
+  fileNo SMALLINT(2) UNSIGNED NOT NULL, # 파일번호 (1)
+  fileDir CHAR(20) NOT NULL, # 파일이 저장되는 폴더명
+  PRIMARY KEY (id),
+  KEY relId (relId,relTypeCode,typeCode,type2Code,fileNo)
+); 
